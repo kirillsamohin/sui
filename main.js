1ba4878de3fa6b52e73c8989f0bb26745158a232
kirillsamohin <97476255+kirillsamohin@users.noreply.github.com>
2024-02-06 12:43:48 +0300

    main.js

--- main.js
+++ main.js
@@ -4,9 +4,10 @@ function calculate() {
   document.getElementById('display').value = result;
 
   // Скрываем калькулятор и показываем видео
-  var calculator = document.querySelector('.calculator');
-  calculator.classList.add('hide');
+  //var calculator = document.querySelector('.calculator');
+  //calculator.classList.add('hide');
 
   var videoContainer = document.getElementById('video-container');
- 
+  //videoContainer.innerHTML = '<video width="560" height="315" controls><source src="52.mp4" type="video/mp4">Ваш браузер не поддерживает тег video.</video>';
+  //videoContainer.style.display = 'block';
 }
