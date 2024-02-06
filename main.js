f549f591b5c38329b2d139147ef9d70553d01ca1
kirillsamohin <97476255+kirillsamohin@users.noreply.github.com>
2024-02-06 12:37:39 +0300

    main.js

--- main.js
+++ main.js
@@ -8,6 +8,5 @@ function calculate() {
   calculator.classList.add('hide');
 
   var videoContainer = document.getElementById('video-container');
-  videoContainer.innerHTML = '<video width="560" height="315" autoplay controls><source src="52.mp4" type="video/mp4">Ваш браузер не поддерживает тег video.</video>';
   videoContainer.style.display = 'block';
 }
