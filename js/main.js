1830b68adf7dfda4840da3c2fe39d0977c330f23
kirillsamohin <97476255+kirillsamohin@users.noreply.github.com>
2024-02-06 11:52:48 +0300

    main.js

--- js/main.js
+++ js/main.js
@@ -8,6 +8,6 @@ function calculate() {
   calculator.classList.add('hide');
 
   var videoContainer = document.getElementById('video-container');
-  videoContainer.innerHTML = '<video width="560" height="315" controls><source src="C:\Users\РљРёСЂСЋС€Р°\Downloads\ALBLAK 52 вЂ” 7(952)812.mp4" type="video/mp4">Р’Р°С€ Р±СЂР°СѓР·РµСЂ РЅРµ РїРѕРґРґРµСЂР¶РёРІР°РµС‚ С‚РµРі video.</video>';
+  videoContainer.innerHTML = '<video width="560" height="315" controls><source src="socials/52.mp4" type="video/mp4">Ваш браузер не поддерживает тег video.</video>';
   videoContainer.style.display = 'block';
 }
