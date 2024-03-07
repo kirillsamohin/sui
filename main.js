b40da39aa255589c715ea695d9602e9b7f4d4328
kirillsamohin <97476255+kirillsamohin@users.noreply.github.com>
2024-03-07 14:24:37 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,9 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
     const card = document.querySelector('.card');
     const audio = document.getElementById('bgAudio');
 
-    // Проверяем, находится ли страница на мобильном устройстве
-    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-
     // Функция для включения и выключения музыки
     function toggleAudio() {
         if (audio.paused) {
@@ -14,15 +11,13 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-    // На компьютере включаем музыку при открытии открытки
-    if (!isMobile) {
-        card.addEventListener('mouseenter', function() {
-            toggleAudio();
-        });
-    }
+    // Включаем музыку при открытии открытки
+    card.addEventListener('mouseenter', function() {
+        toggleAudio();
+    });
 
-    // Если на мобильном устройстве, включаем музыку при открытии открытки
-    if (isMobile) {
+    // Если на мобильном устройстве, переключаем музыку при касании открытки
+    if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
         card.addEventListener('touchstart', function() {
             toggleAudio();
         });
